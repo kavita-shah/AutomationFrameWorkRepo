1904b657982e689de62a426ebf584c41aa72cf75
kavita-shah <https://github.com/kavita-shah/VtigerAppFw.git>
2025-07-19 12:42:54 +0530

    FrameWork commited

---
+++ AutomationFrameWork11/src/main/java/GenericUtility/BaseClass.java
@@ -0,0 +1,11 @@
+package GenericUtility;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("BaseClass is created");
+
+	}
+
+}
---
+++ AutomationFrameWork11/src/test/java/CreateContactTest/CreateConatctWithPhone.java
@@ -0,0 +1,11 @@
+package CreateContactTest;
+
+public class CreateConatctWithPhone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("conatct is created with phone");
+
+	}
+
+}
