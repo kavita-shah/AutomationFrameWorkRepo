64a7e77797c0d2a57c1f01f7348226230ac4dccb
kavita-shah <https://github.com/kavita-shah/VtigerAppFw.git>
2025-07-21 14:46:20 +0530

    added class

---
+++ AutomationFrameWork11/src/main/java/GenericUtility/DataBaseUtility.java
@@ -0,0 +1,11 @@
+package GenericUtility;
+
+public class DataBaseUtility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dadabase Is connected");
+
+	}
+
+}
