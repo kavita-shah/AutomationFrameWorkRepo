6bd87306f54ec2e7a99466585099fb29371e9d8f
kavita-shah <https://github.com/kavita-shah/VtigerAppFw.git>
2025-07-21 15:25:53 +0530

    Add element

--- AutomationFrameWork11/src/main/java/GenericUtility/BaseClass.java
+++ AutomationFrameWork11/src/main/java/GenericUtility/BaseClass.java
@@ -5,6 +5,8 @@ public class BaseClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("BaseClass is created");
+	
+	System.out.println("Methods is written");
 
 	}
 
