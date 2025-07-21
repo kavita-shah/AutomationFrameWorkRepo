86170a722920853f00e53fc8d4cda03fa329e4f4
kavita-shah <https://github.com/kavita-shah/VtigerAppFw.git>
2025-07-21 23:04:44 +0530

    Class is added in fw

--- AutomationFrameWork11/src/main/java/GenericUtility/CreateOrgPage.java
+++ AutomationFrameWork11/src/main/java/GenericUtility/CreateOrgPage.java
@@ -1,5 +1,17 @@
 package GenericUtility;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CreateOrgPage {
+	@FindBy(name="//input[@alt='Create organization...']")
+	private WebElement createContacredt;
+
+	public WebElement getCreateContacredt() {
+		return createContacredt;
+	}
+	
+	
+	
 
 }
